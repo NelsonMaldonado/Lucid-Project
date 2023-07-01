9ba885cf0c1c57846eed1f09a5efa94b25b4067d
Nelson Maldonado <play2nino@gmail.com>
2023-07-01 10:12:47 -0700

    added time stamp

--- assets/components/manualButton.js
+++ assets/components/manualButton.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { View, Text, StyleSheet } from "react-native"
 import { Button } from "@react-native-material/core"
 
 const MyButton = () => {
   const handlePress = () => {
-    alert("You pressed manual sleep")
+    const now = new Date()
+    formattedTime = now.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+    console.log(now)
   }
-  return <Button onPress={handlePress} title="SLEEP"></Button>
+
+  return (
+    <View>
+      <Button onPress={handlePress} title="SLEEP"></Button>
+      <Text style={styles.texto}>{formattedTime}</Text>
+    </View>
+  )
 }
 
 export default MyButton
+
+const styles = StyleSheet.create({
+  texto: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+})
